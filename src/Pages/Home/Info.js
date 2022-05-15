e88acb5a114ea31fe8c44ce8d23ddbbb23929f13
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-16 02:16:28 +0600

    info dynamic title, body and dynamic class added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,30 @@
 import React from "react";
 import InfoCard from "./InfoCard";
 import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-20 my-10">
-      <InfoCard img={clock}></InfoCard>
-      <InfoCard img={clock}></InfoCard>
-      <InfoCard img={clock}></InfoCard>
+      <InfoCard
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={clock}
+        cardTitle="Opening Hours"
+        cardBody="9:00 Am to 5:00 PM"
+      ></InfoCard>
+      <InfoCard
+        bgClass="bg-accent"
+        img={marker}
+        cardTitle="Our Location"
+        cardBody="Brooklyn, NY 10036, United States"
+      ></InfoCard>
+      <InfoCard
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={phone}
+        cardTitle="Contact Us"
+        cardBody="+000 123 456789"
+      ></InfoCard>
     </div>
   );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ bgClass, img, cardTitle, cardBody }) => {
   return (
-    <div class="card lg:card-side text-white shadow-xl bg-gradient-to-r from-secondary to-primary mx-3 px-5 py-5 text-center">
+    <div
+      class={`card lg:card-side text-white shadow-xl  mx-3 px-5 py-5 ${bgClass}`}
+    >
       <figure>
         <img src={img} alt="Album" />
       </figure>
-      <div class="card-body">
-        <h2 class="card-title mx-auto">New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
+      <div class="card-body text-center">
+        <h2 class="card-title mx-auto">{cardTitle}</h2>
+        <p>{cardBody}</p>
       </div>
     </div>
   );
