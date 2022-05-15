72660e86f8dac6b735afb1a9b1824c65697e5980
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-16 03:50:30 +0600

    service area done

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Service from "./Service";
+import fluoride from "../../assets/images/fluoride.png";
+import cavity from "../../assets/images/cavity.png";
+import whitening from "../../assets/images/whitening.png";
+
+const Services = () => {
+  const services = [
+    {
+      _id: 1,
+      name: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: "Cavity Feeling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: cavity,
+    },
+    {
+      _id: 3,
+      name: "Treeth Witening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: whitening,
+    },
+  ];
+  return (
+    <div className="mt-32 mb-5 pb-20">
+      <div className="text-center">
+        <h3 className="text-xl font-bold text-secondary">Our Services</h3>
+        <h2 className="text-4xl font-medium text-accent">
+          Services We Provide
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10  my-10">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
