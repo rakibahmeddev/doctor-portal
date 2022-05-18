d13d7bb89020497faf254645a67defb52a4854bd
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-18 13:04:33 +0600

    Booking Modal done

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -27,7 +27,13 @@ const AvailableAppointments = ({ date }) => {
           ></Service>
         ))}
       </div>
-      {treatment && <BookingModal treatment={treatment}></BookingModal>}
+      {treatment && (
+        <BookingModal
+          date={date}
+          treatment={treatment}
+          setTreatment={setTreatment}
+        ></BookingModal>
+      )}
     </div>
   );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { format } from "date-fns";
+const BookingModal = ({ date, treatment, setTreatment }) => {
+  const { _id, name, slots } = treatment;
 
-const BookingModal = ({ treatment }) => {
-  const { name, slots } = treatment;
+  const handleBooking = (event) => {
+    event.preventDefault();
+    const slot = event.target.slot.value;
+    const serviceName = event.target.name.value;
+    const phone = event.target.phone.value;
+    const email = event.target.email.value;
+    console.log(_id, name, slot, serviceName, phone, email);
+    setTreatment(null);
+  };
   return (
     <div>
       <input type="checkbox" id="booking-modal" class="modal-toggle" />
@@ -13,16 +23,51 @@ const BookingModal = ({ treatment }) => {
           >
             ✕
           </label>
-          <h3 class="font-bold text-lg text-secondary">Booking for: {name}</h3>
-          <p class="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div class="modal-action">
-            <label for="booking-modal" class="btn">
-              Yay!
-            </label>
+          <h3 class="font-bold text-xl text-secondary text-center mb-5">
+            Booking for: {name}
+          </h3>
+          <div>
+            <form
+              onSubmit={handleBooking}
+              className="grid grid-cols-1 gap-5 justify-items-center "
+            >
+              <input
+                type="text"
+                disabled
+                value={format(date, "PP")}
+                class="input input-bordered bg-[#E6E6E6] w-full "
+              />
+              <select name="slot" class="select bg-[#E6E6E6]  w-full ">
+                {slots.map((slot) => (
+                  <option value={slot}>{slots}</option>
+                ))}
+              </select>
+              <input
+                name="name"
+                type="text"
+                placeholder="Full Name"
+                class="input input-bordered w-full "
+              />
+              <input
+                name="phone"
+                type="text"
+                placeholder="Phone Number"
+                class="input input-bordered w-full "
+              />
+              <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                class="input input-bordered w-full "
+              />
+              <input
+                type="submit"
+                value="Submit"
+                class="btn btn-secondary text-white w-full "
+              />
+            </form>
           </div>
+          <div class="modal-action"></div>
         </div>
       </div>
     </div>
