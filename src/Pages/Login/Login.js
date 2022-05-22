0358df8852968f68bac6efd54c38b78cc3824142
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-22 12:39:46 +0600

    error handle and display loading spinner

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,26 +1,48 @@
 import React from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
+import Loading from "../Shared/Loading";
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
 
-  if (user) {
-    console.log(user);
+  let signInError;
+
+  if (loading || gLoading) {
+    return <Loading></Loading>;
+  }
+
+  if (error || gError) {
+    signInError = (
+      <p className="text-red-500 mb-3">
+        <small>{error?.message || gError?.message}</small>
+      </p>
+    );
+  }
+
+  if (gUser) {
+    console.log(gUser);
   }
 
   const onSubmit = (data) => {
     console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
   };
 
   return (
-    <div className="my-40 flex justify-center items-center">
+    <div className="my-20 flex justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-2xl font-bold text-center">Login</h2>
@@ -93,6 +115,8 @@ const Login = () => {
                   )}
                 </label>
               </div>
+
+              {signInError}
 
               <input
                 className="btn text-white w-full max-w-xs"
