01f1e71370603ed628421e8fb7cbae2137774b1c
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-24 00:26:00 +0600

    Require Auth and redirect user to the page they wanted to go

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,7 +6,7 @@ import {
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -20,6 +20,11 @@ const Login = () => {
   } = useForm();
 
   let signInError;
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
 
   if (loading || gLoading) {
     return <Loading></Loading>;
@@ -35,6 +40,7 @@ const Login = () => {
 
   if (user || gUser) {
     console.log(user || gUser);
+    navigate(from, { replace: true });
   }
 
   const onSubmit = (data) => {
